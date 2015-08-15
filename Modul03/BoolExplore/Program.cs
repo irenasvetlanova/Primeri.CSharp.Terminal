119b7a8189366f88c55e8238154d3870e68dee03
Ирена <irena.svetlanova@gmail.com>
2015-08-16 01:10:00 +0300

    Модул 03, Видео 04 - Променливи bool

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -6,7 +6,21 @@ namespace BoolExplore
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+
+			//bool _b = true;  //true / false
+
+			//bool _test = 5 > 10;
+
+			int[] _i = new int[] { 4, 3, 2 };
+
+			int _ui = Convert.ToInt32 (Console.ReadLine ());
+
+			bool _check01 = _ui <= _i.Length;
+			bool _check02 = _ui > 0;
+
+			Console.WriteLine ( _check01 );
+			Console.WriteLine ( _check02 );
+			Console.WriteLine ( _i [ _ui - 1]);
 		}
 	}
 }
