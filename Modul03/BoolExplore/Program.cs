2780b2e8dd9bc6c6a4e707afc353874eaf1233f2
Ирена <irena.svetlanova@gmail.com>
2015-08-17 23:54:44 +0300

    Модул 03, Видео 07 - Примери с променливи bool -2

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -13,11 +13,37 @@ namespace BoolExplore
 
 			int[] _i = new int[] { 4, 3, 2 };
 
-			int _ui = Convert.ToInt32 (Console.ReadLine ());
-//
-//			bool _check01 = _ui <= _i.Length;
-//			bool _check02 = _ui > 0;
-//
+			int _ui = 0;//Convert.ToInt32 (Console.ReadLine ());
+
+		
+			string _input = "0";
+
+			Console.Write ("Моля въведете индекс:");
+			_input = Console.ReadLine ();
+
+			bool _check01 = int.TryParse (_input, out _ui);
+			bool _check02 = _ui <= _i.Length;
+			bool _check03 = _ui > 0;
+
+			if (_check01 && _check02 && _check03) {
+				Console.WriteLine (_i [_ui - 1]);
+			}
+
+			if (!_check01) {
+				Console.WriteLine ("Индекса трябва да е цяло число\n\n\n");
+
+			}
+
+			if (!_check02) {
+				Console.WriteLine ("Индекса е прекалено голям\n\n\n");
+
+			}
+
+			if (!_check03 && _check01) {
+				Console.WriteLine ("Индекса е прекалено малък\n\n\n");
+
+			}
+
 //			bool _exp01 = _check01 && _check02;     //Логическо и - "&&"
 //			bool _exp02 = _check01 || _check02;     //Логическо и - "||"
 //			bool _exp03 = !_check01;				//Отрицание "!"
@@ -26,23 +52,23 @@ namespace BoolExplore
 //			Console.WriteLine ( "Логическо ИЛИ:" + _exp02.ToString () );
 //			Console.WriteLine ("Отрицание на " + _check01.ToString () + ": " + _exp03);
 
-			if (_ui > 0 && _ui <= _i.Length) {
-				Console.WriteLine (_i [_ui - 1]);
-			} else {
-				if (_ui > _i.Length) 
-				{
-					Console.WriteLine ("Индекса е прекалено голям\n\n\n");
-				}
-
-				if (_ui <= 0) 
-				{
-					Console.WriteLine ("Индекса е прекалено малък\n\n\n");
-				}
-				Console.WriteLine (_i [_ui - 1]);
+//			if (_ui > 0 && _ui <= _i.Length) {
+//				Console.WriteLine (_i [_ui - 1]);
+//			} else {
+//				if (_ui > _i.Length) 
+//				{
+//					Console.WriteLine ("Индекса е прекалено голям\n\n\n");
+//				}
+//
+//				if (_ui <= 0) 
+//				{
+//					Console.WriteLine ("Индекса е прекалено малък\n\n\n");
+//				}
+//				Console.WriteLine (_i [_ui - 1]);
 			}
 		}
 	}
-}
+
 
 		
 	
