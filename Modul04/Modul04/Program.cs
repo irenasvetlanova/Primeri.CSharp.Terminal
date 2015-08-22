ae7a729a2f3f2a78c644d5a345e05a20573f7196
Ирена <irena.svetlanova@gmail.com>
2015-08-22 23:01:45 +0300

    Модул 04, ВИдео 02 - примери

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -6,10 +6,17 @@ namespace Modul04
 	{
 		public static void Main (string[] args)
 		{
-			for (int i = 0; i <= 5; i += 2) 
+			string[] _table = { "Иван", "Гошо", "Гергана" };
+
+			//Антетка на таблица
+			Console.WriteLine ("Име");
+			Console.WriteLine ("==========");
+
+			//Тяло на таблица
+			for (int i = 0; i < _table.Length; i++) 
 			{
 				//Код на цикъла
-				Console.WriteLine (i.ToString ());
+				Console.WriteLine ( _table [i]);
 			}
 		}
 	}
