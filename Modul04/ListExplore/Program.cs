8f9b014ba4160dd3e151629ddf065ec009c6df7f
Ирена <irena.svetlanova@gmail.com>
2015-08-23 21:20:35 +0300

    Модул 04, Видео 07 - List

--- Modul04/ListExplore/Program.cs
+++ Modul04/ListExplore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExplore
 {
@@ -8,12 +9,36 @@ namespace ListExplore
 		{
 			//Дефиниции
 			string _userinput = "";
+			List<int> _list = new List<int> ();
+
+			_list.Add (5);
+			_list.Add (3);
+			_list.Add (9);
 
 			//Потребителски вход
 			do {
 				Console.Write ("? ");
 				_userinput = Console.ReadLine ();
 
+				//Добаване на стойности
+
+				//Преглед на List
+				if ( _userinput.Contains ("show") )
+				{
+					Console.Write ( "_list: ");
+
+					for (int i = 0; i < _list.Count; i++ )
+					{
+						
+						Console.Write ( _list [i] );
+
+						if ( i != _list.Count - 1 ) Console.Write (", ");
+				}
+
+				//Размер на List
+
+
+				}
 			} while(_userinput != "exit");
 		}
 	}
