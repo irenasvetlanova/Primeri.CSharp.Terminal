86d92fc80c29fa8ca42cd9e55c6d06b9855c760b
Ирена <irena.svetlanova@gmail.com>
2015-08-23 21:54:36 +0300

    Модул 04, Видео 08 - Примери List

--- Modul04/ListExplore/Program.cs
+++ Modul04/ListExplore/Program.cs
@@ -21,6 +21,24 @@ namespace ListExplore
 				_userinput = Console.ReadLine ();
 
 				//Добаване на стойности
+				//add <int>
+				if ( _userinput.ToLower ().Contains ("add"))
+				{
+					try 
+					{
+						
+						int _add = 0;
+
+						if (int.TryParse ( _userinput.Split (' ')[1], out _add ) )
+						{
+							_list.Add ( _add );
+						}
+
+						Console.WriteLine ();
+					}catch{}
+				}
+
+
 
 				//Преглед на List
 				if ( _userinput.Contains ("show") )
@@ -33,10 +51,17 @@ namespace ListExplore
 						Console.Write ( _list [i] );
 
 						if ( i != _list.Count - 1 ) Console.Write (", ");
+
+					}
+
+						Console.WriteLine ("\n");
+
 				}
 
 				//Размер на List
-
+				if ( _userinput.ToLower().Contains ("size"))
+				{
+					Console.WriteLine ( "Размер на списъка: " + _list.Count.ToString () + "\n");
 
 				}
 			} while(_userinput != "exit");
