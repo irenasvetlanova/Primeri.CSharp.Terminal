ad8dda4f45bb433f21091b52430e580ddd6f582a
Ирена <irena.svetlanova@gmail.com>
2015-10-04 11:48:58 +0300

    Модул 05 Видео 1 Въведение в методите

--- Modul04/ListExplore/Program.cs
+++ Modul04/ListExplore/Program.cs
@@ -41,18 +41,22 @@ namespace ListExplore
 
 
 				//Преглед на List
-				if ( _userinput.Contains ("show") )
+				if ( _userinput.ToLower().Contains ("show") )
 				{
-					Console.Write ( "_list: ");
+					Console.Write ( "Списък: ");
 
-					for (int i = 0; i < _list.Count; i++ )
+					foreach (int value in _list)
 					{
-						
-						Console.Write ( _list [i] );
-
-						if ( i != _list.Count - 1 ) Console.Write (", ");
-
+						Console.Write (value.ToString () + ", ");
 					}
+//					for (int i = 0; i < _list.Count; i++ )
+//					{
+//						
+//						Console.Write ( _list [i] );
+//
+//						if ( i != _list.Count - 1 ) Console.Write (", ");
+//
+//					}
 
 						Console.WriteLine ("\n");
 
