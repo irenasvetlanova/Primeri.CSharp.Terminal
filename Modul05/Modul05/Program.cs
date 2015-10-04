7c6abc82ac56ae57bc6fae5eeda007b1c7f105e2
Ирена <irena.svetlanova@gmail.com>
2015-10-04 14:31:50 +0300

    Модул 5 Видео 03 Използване на масиви

--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -42,6 +42,11 @@ namespace Modul05
 					}
 
 				}
+
+				if (usercommand.Contains ("cmd05"))
+				{
+					Console.WriteLine ("Брой четни числа: " + cmd05 (usercommand).Length.ToString ());
+				}
 					
 			} while (usercommand != "exit");
 
@@ -94,6 +99,29 @@ namespace Modul05
 			_i = _i * _i;
 
 			return (_temp > 0);
+		}
+
+		public static string[]        cmd05 ( string _input )
+		{
+			try {
+				int _border = 0;
+				string _temp = "";
+
+				if (int.TryParse (_input.Split (' ') [1], out _border)) 
+				{
+					for (int i = 0; i < _border; i++) 
+					{
+						if (i % 2 == 0)							_temp += i.ToString ();
+						if (i != _border - 1 && i % 2 == 0)					_temp += ",";
+					}
+					Console.WriteLine (_temp);      //само за проверка
+
+					return _temp.Split (',');
+				}
+
+			} catch {
+			}
+			return new string[1] {"0"};
 		}
 
 		}
